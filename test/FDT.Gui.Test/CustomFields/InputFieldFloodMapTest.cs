bb97757febd9a233edb0b82565a3d99ea0be26eb
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-08-11 17:42:20 +0200

    Fixed errors with the test environment.

---
+++ test/FDT.Gui.Test/CustomFields/InputFieldFloodMapTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using FDT.Gui.CustomFields;
+using System;
+using System.Threading;
+using System.Windows;
+using FDT.TestUtils;
+
+namespace FDT.Gui.Test.CustomFields
+{
+    [TestFixture, Apartment(ApartmentState.STA)]
+    class InputFieldFloodMapTest
+    {
+        [Test]
+        [STAThread]
+        public void TestInputFieldAsGui()
+        {
+            InputFieldFloodMap inputField = new InputFieldFloodMap();
+            WpfTestHelper testHelper = new WpfTestHelper(inputField, "Test", () => { });
+            testHelper.ShowDialog();
+        }
+    }
+}
