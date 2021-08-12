852c17f2026ee69df2c6fd96cc1e95298e1dcead
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-08-12 16:45:43 +0200

    Added logic to read t he directories as basin structures.

--- src/FDT.Gui/MainWindow.xaml.cs
+++ src/FDT.Gui/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32;
 
 namespace FDT.Gui
 {
@@ -10,6 +15,28 @@ namespace FDT.Gui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ParentControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var availableBasins = BrowseExposureDirectory(sender, e).ToArray();
+            ViewModel?.LoadBasins?.Execute(availableBasins);
+        }
+
+        private IEnumerable<string> BrowseExposureDirectory(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new FolderBrowserDialog();
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.SelectedPath))
+            {
+                var directories = Directory.GetDirectories(openFileDialog.SelectedPath);
+                foreach (string directory in directories)
+                {
+                    yield return directory.Remove(0, directory.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+                }
+            }
         }
     }
 }
--- src/FDT.Gui/ViewModels/MainWindowViewModel.cs
+++ src/FDT.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using FDT.Gui.Commands;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace FDT.Gui.ViewModels
 {
@@ -13,11 +15,20 @@ namespace FDT.Gui.ViewModels
             
             BasinScenarios.Add(EventBasedScenario);
             BasinScenarios.Add(RiskBasedScenario);
-        }
+
+            LoadBasins = new RelayCommand(OnLoadBasins);
+    }
         
         public ObservableCollection<IBasinScenario> BasinScenarios { get; }
 
         public IBasinScenario EventBasedScenario { get; }
         public IBasinScenario RiskBasedScenario { get; }
+
+        public ICommand LoadBasins { get; }
+
+        private void OnLoadBasins(object objectCmd)
+        {
+
+        }
     }
 }
