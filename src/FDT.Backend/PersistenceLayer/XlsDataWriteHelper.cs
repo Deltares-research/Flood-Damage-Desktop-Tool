afd13db49365bd15725f597028547c8a3c5a293a
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-09-06 18:21:11 +0200

    FPT-129 Added validation for the basin and related test case data. (#13)

--- src/FDT.Backend/PersistenceLayer/XlsDataWriteHelper.cs
+++ src/FDT.Backend/PersistenceLayer/XlsDataWriteHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using ClosedXML.Excel;
+using FDT.Backend.DomainLayer.IDataModel;
 
 namespace FDT.Backend.PersistenceLayer
 {
@@ -14,6 +16,20 @@ namespace FDT.Backend.PersistenceLayer
             IXLWorksheet hazardWorksheet;
             workBook.Worksheets.TryGetWorksheet(tabName, out hazardWorksheet);
             return hazardWorksheet;
+        }
+
+        public static void ValidateBasinData(this IBasin basinData)
+        {
+            if (basinData == null)
+                throw new ArgumentNullException(nameof(basinData));
+            if (string.IsNullOrEmpty(basinData.BasinName))
+                throw new ArgumentNullException(nameof(basinData.BasinName));
+            if (string.IsNullOrEmpty(basinData.Projection))
+                throw new ArgumentNullException(nameof(basinData.Projection));
+            if (basinData.Scenarios == null || !basinData.Scenarios.Any())
+                throw new Exception("No valid scenarios were provided.");
+            if (basinData.Scenarios.Any(s => string.IsNullOrEmpty(s.ScenarioName)))
+                throw new Exception("All selected scenarios should contain a valid name.");
         }
     }
 }
