54c6f266d9264995d82fa242b3f9a33e6a40393c
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-09-15 16:11:38 +0200

    FPT-134 Added error handling for BaseFloodMap Returnperiod.

--- src/FDT.Gui/CustomFields/InputFieldFloodMap.xaml.cs
+++ src/FDT.Gui/CustomFields/InputFieldFloodMap.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using FDT.Gui.ViewModels;
 using Microsoft.Win32;
 
---
+++ test/FDT.Gui.Test/ViewModels/BaseFloodMapTest.cs
@@ -0,0 +1,31 @@
+﻿using FDT.Gui.ViewModels;
+using NUnit.Framework;
+
+namespace FDT.Gui.Test.ViewModels
+{
+    public class BaseFloodMapTest
+    {
+        [Test]
+        public void TestFloodMapHasValidReturnPeriod()
+        {
+            var floodMap = new FloodMap();
+            Assert.That(floodMap, Is.Not.Null);
+            Assert.That(floodMap, Is.InstanceOf<IFloodMap>());
+            Assert.That(floodMap.HasReturnPeriod, Is.False);
+            Assert.That(floodMap[nameof(floodMap.ReturnPeriod)], Is.EqualTo(null));
+        }
+
+        [Test]
+        [TestCase(0, "Return Period should be greater than 0")]
+        [TestCase(42, null)]
+        public void TestFloodMapWithReturnPeriodValidationWorks(int returnPeriod, string error)
+        {
+            var floodMap = new FloodMapWithReturnPeriod();
+            floodMap.ReturnPeriod = returnPeriod;
+            Assert.That(floodMap, Is.Not.Null);
+            Assert.That(floodMap, Is.InstanceOf<IFloodMap>());
+            Assert.That(floodMap.HasReturnPeriod, Is.True);
+            Assert.That(floodMap[nameof(floodMap.ReturnPeriod)], Is.EqualTo(error));
+        }
+    }
+}
