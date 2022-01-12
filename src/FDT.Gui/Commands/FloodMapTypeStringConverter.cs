a02b70348d29d996a01dfa029b1f9fbe41cc0d3e
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-01-12 09:33:46 +0100

    FPT-142 added converter for display labels

--- src/FDT.Gui/Commands/FloodMapTypeStringConverter.cs
+++ src/FDT.Gui/Commands/FloodMapTypeStringConverter.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Windows.Data;
 using System.Windows.Markup;
 using FDT.Backend.DomainLayer.IDataModel;
 
 namespace FDT.Gui.Commands
 {
-    [ValueConversion(typeof(Enum), typeof(IEnumerable<string>))]
+
     public class FloodMapTypeStringConverter: MarkupExtension, IValueConverter
     {
         private T Convert<T>(object o)
@@ -16,25 +14,12 @@ namespace FDT.Gui.Commands
             T enumVal = (T)Enum.ToObject(typeof(T), o);
             return enumVal;
         }
-
-        private IEnumerable<string> ConvertEnumCollection(IEnumerable<FloodMapType> valueCollection)
-        {
-            return valueCollection.Select(mapType => Convert<FloodMapType>(mapType).GetDisplayName());
-        }
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            switch (value)
-            {
-                case null:
-                    throw new ArgumentNullException(nameof(value));
-                case string stringValue:
-                    return ConvertBack(stringValue, targetType, parameter, culture);
-                case IEnumerable<FloodMapType> enumValues:
-                    return ConvertEnumCollection(enumValues);
-                default:
-                    throw new ArgumentNullException(nameof(value));
-            }
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            FloodMapType floodMapType = Convert<FloodMapType>(value);
+            return floodMapType.GetDisplayName();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
