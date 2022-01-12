202a2ae3a13ff7fd510fddbaa503aef509c4c20a
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-01-12 07:36:05 +0100

    FPT-142 Created separate control for scenarios input

---
+++ src/FDT.Gui/CustomFields/ScenarioInputControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FDT.Gui.ViewModels;
+
+namespace FDT.Gui.CustomFields
+{
+    /// <summary>
+    /// Interaction logic for ScenarioInputControl.xaml
+    /// </summary>
+    public partial class ScenarioInputControl : UserControl
+    {
+        public static readonly DependencyProperty ParameterProperty =
+            DependencyProperty.Register(
+                "Scenario",
+                typeof(IScenario),
+                typeof(ScenarioInputControl),
+                new PropertyMetadata(null));
+
+        public ScenarioInputControl()
+        {
+            InitializeComponent();
+        }
+
+        public IScenario Scenario
+        {
+            get => (IScenario) GetValue(ParameterProperty);
+            set => SetValue(ParameterProperty, value);
+        }
+    }
+}
